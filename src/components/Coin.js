96dcefc4a4fa3f22763e43d0970a420f93c13b19
Fábio Rodrigues <88008767+rochafrr@users.noreply.github.com>
2022-11-10 20:52:20 -0300

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-function Coin({ TotalSupply, CirculatingSupply, Staking, BlockHeight, Burn }) { 
+function Coin({ TotalSupply, Pool, Staking, BlockHeight, Burn }) { 
  
     return ( 
       <div className="coin"> 
@@ -12,16 +12,16 @@ function Coin({ TotalSupply, CirculatingSupply, Staking, BlockHeight, Burn }) {
           <h3>Total Supply:<br></br>{parseFloat(TotalSupply).toLocaleString('pt-br', {minimumFractionDigits: 2})} LUNC</h3> 
         </div> 
    
-        <div className="CSupply"> 
-          <h3> Circ. Supply: {parseFloat((TotalSupply)-(Staking/1000000)).toLocaleString('pt-br', {minimumFractionDigits: 2})} LUNC</h3> 
-        </div> 
+       <div className="pool" >
+        <h3> Community Pool: {parseFloat(Pool / 1000000).toLocaleString()} LUNC</h3>
+       </div>
    
         <div className="Staking"> 
           <h3>Pool Staking: {(Staking/1000000).toLocaleString()} LUNC</h3> 
         </div> 
    
         <div className="Block"> 
-        <h3>Block Height:{BlockHeight}</h3> 
+        <h3>Block Height:{parseInt(BlockHeight).toLocaleString()}</h3> 
         </div> 
    
      
